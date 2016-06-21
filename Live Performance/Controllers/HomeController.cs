c3942191b166029949022304858bbe275317bc23
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-06-21 17:55:21 +0200

    Add empty project

---
+++ Live Performance/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Live_Performance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Live Performance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Live_Performance.MvcApplication" Language="C#" %>
