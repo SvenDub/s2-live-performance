c5cab8c70f2f24246138194e48a02bbed22deab5
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-22 15:21:02 +0200

    Scaffold budget

--- Live Performance/Controllers/BudgetController.cs
+++ Live Performance/Controllers/BudgetController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Web.Http;
+using Live_Performance.Entity;
+using Live_Performance.Models;
 
 namespace Live_Performance.Controllers
 {
     public class BudgetController : ApiController
     {
+        public int Lakes(Rent rent, int budget) => Budget.LakesForBudget(rent, budget);
     }
 }
