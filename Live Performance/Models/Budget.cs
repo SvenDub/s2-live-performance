3a2dcb98e9aebb349c319019a5c82c5a1588d2af
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-22 15:33:33 +0200

    Add WindChill tests

---
+++ Live Performance/Models/Budget.cs
@@ -0,0 +1,12 @@
+﻿using Live_Performance.Entity;
+
+namespace Live_Performance.Models
+{
+    public class Budget
+    {
+        public static int LakesForBudget(Rent rent, int budget)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Util.Tests/TemperatureTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Util.Tests
+{
+    public class TemperatureTest
+    {
+        [Test]
+        public void TestWindChill()
+        {
+            Assert.AreEqual(27.87, Temperature.WindChill(28, 2));
+            Assert.AreEqual(27.38, Temperature.WindChill(28, 3));
+            Assert.AreEqual(25.80, Temperature.WindChill(27, 4));
+            Assert.AreEqual(17.36, Temperature.WindChill(0, 0));
+            Assert.AreEqual(1.78, Temperature.WindChill(10, 7));
+            Assert.AreEqual(29.21, Temperature.WindChill(30, 5));
+            Assert.AreEqual(-21.34, Temperature.WindChill(-20, 2));
+            Assert.AreEqual(4.76, Temperature.WindChill(1, 1));
+            Assert.AreEqual(2.99, Temperature.WindChill(-1, 1));
+        }
+
+        [Test]
+        public void TestWindChillNegative()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Temperature.WindChill(1, -1));
+        }
+    }
+}
