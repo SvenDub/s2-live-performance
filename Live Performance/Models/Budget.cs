46e449cf69ff380a61021683251049b8ef7c9784
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-22 16:27:02 +0200

    Calculate lakes available

--- Live Performance/Models/Budget.cs
+++ Live Performance/Models/Budget.cs
@@ -1,12 +1,55 @@
-﻿using Live_Performance.Entity;
+﻿using System;
+using System.Linq;
+using Live_Performance.Entity;
 
 namespace Live_Performance.Models
 {
     public class Budget
     {
+        private const int AvailableLakes = 12;
+        private const double BaseLakeCost = 100;
+        private const double AdditionalLakeCost = 50;
+        private const int AdditionalLakeThreshold = 5;
+
         public static int LakesForBudget(Rent rent, int budget)
         {
-            return 0;
+            // Amount of days, same day counts as 1
+            int days = (rent.End - rent.Begin).Days + 1;
+
+            // Costs for boats (rent only)
+            int boatCosts = rent.Boats.Sum(boatRent => boatRent.Cost)*days;
+            int boatAmount = rent.Boats.Count;
+
+            // Costs for areas
+            int areaCosts = rent.Areas.Sum(areaRent => areaRent.Cost)*boatAmount*days;
+
+            // Cost for additional articles
+            int articleCosts = rent.Articles.Sum(articleRent => articleRent.Cost*articleRent.Amount)*days;
+
+            // Budget left for lakes
+            int availableBudget = budget - boatCosts - areaCosts - articleCosts;
+
+            // Costs per lake
+            double lakeCost;
+
+            // Can afford enough to pay additional lake costs
+            bool overThreshold = availableBudget >=
+                                 (BaseLakeCost + AdditionalLakeCost)*(AdditionalLakeThreshold + 1)*boatAmount*days;
+
+            if (overThreshold)
+            {
+                lakeCost = BaseLakeCost + AdditionalLakeCost;
+            }
+            else
+            {
+                lakeCost = BaseLakeCost;
+            }
+
+            // Divide the available budget over the lakes
+            int lakes = (int) Math.Floor(availableBudget/(lakeCost*boatAmount*days));
+
+            // Bound the amount of lakes by the maximum available and the threshold
+            return Math.Min(overThreshold ? AvailableLakes : AdditionalLakeThreshold, lakes);
         }
     }
 }
