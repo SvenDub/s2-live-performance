9a68d2432c2ab6b37875f5593a2e556410caeb56
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-23 14:07:06 +0200

    Show lakes before saving

--- Live Performance/Controllers/BudgetController.cs
+++ Live Performance/Controllers/BudgetController.cs
@@ -1,11 +1,42 @@
 ﻿using System.Web.Http;
+using Inject;
 using Live_Performance.Entity;
 using Live_Performance.Models;
+using Live_Performance.Persistence;
 
 namespace Live_Performance.Controllers
 {
     public class BudgetController : ApiController
     {
-        public int Lakes(Rent rent, int budget) => Budget.LakesForBudget(rent, budget);
+        private readonly IRepository<Area> _areaRepository = Injector.Resolve<IRepository<Area>>();
+        private readonly IRepository<Article> _articleRepository = Injector.Resolve<IRepository<Article>>();
+        private readonly IRepository<Boat> _boatRepository = Injector.Resolve<IRepository<Boat>>();
+
+        [HttpPost]
+        public int Index(Rent rent)
+        {
+            rent.Articles.RemoveAll(articleRent => articleRent.Article.Id == -1 || articleRent.Amount <= 0);
+            rent.Articles.ForEach(articleRent =>
+            {
+                articleRent.Article = _articleRepository.FindOne(articleRent.Article.Id);
+                articleRent.Cost = articleRent.Article.Cost;
+            });
+
+            rent.Boats.RemoveAll(boatRent => boatRent.Boat.Id == -1);
+            rent.Boats.ForEach(boatRent =>
+            {
+                boatRent.Boat = _boatRepository.FindOne(boatRent.Boat.Id);
+                boatRent.Cost = boatRent.Boat.BoatType.Cost;
+            });
+
+            rent.Areas.RemoveAll(areaRent => areaRent.Area.Id == -1);
+            rent.Areas.ForEach(areaRent =>
+            {
+                areaRent.Area = _areaRepository.FindOne(areaRent.Area.Id);
+                areaRent.Cost = areaRent.Area.Cost;
+            });
+
+            return Budget.LakesForBudget(rent, rent.Budget);
+        }
     }
 }
--- Live Performance/Global.asax.cs
+++ Live Performance/Global.asax.cs
@@ -1,13 +1,15 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Live_Performance.Peristence.Oracle;
 using Live_Performance.Persistence;
+using Newtonsoft.Json;
 
 namespace Live_Performance
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -18,6 +20,13 @@ namespace Live_Performance
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             PersistenceInjector.Inject(new OracleRepositoryProvider());
+
+            // Include 0, false, null, etc
+            GlobalConfiguration.Configuration
+                .Formatters
+                .JsonFormatter
+                .SerializerSettings
+                .DefaultValueHandling = DefaultValueHandling.Include;
         }
     }
 }
