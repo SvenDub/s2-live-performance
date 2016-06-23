e9a775b4a0d8204469c4c2c35f3d41dfc302b964
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-23 16:35:43 +0200

    Show WindChill

---
+++ Live Performance/Controllers/ArticleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Live_Performance.Entity;
+using Live_Performance.Models;
+
+namespace Live_Performance.Controllers
+{
+    public class ArticleController : EntityController<Article>
+    {
+        public ActionResult Index() => View(Repository.FindAll());
+
+        [Authentication(Admin = true)]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authentication(Admin = true)]
+        public ActionResult New(Article article)
+        {
+            article.Id = 0;
+            article.Cost = 125;
+
+            Article saved = Repository.Save(article);
+            return RedirectToAction("Index", "Article");
+        }
+
+        [Authentication(Admin = true)]
+        public ActionResult Delete(int id)
+        {
+            Repository.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Util/Temperature.cs
+++ Util/Temperature.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Util
 {
@@ -17,6 +22,42 @@ namespace Util
                 throw new ArgumentOutOfRangeException(nameof(wind), wind, "Wind speed cannot be negative");
             }
             return Math.Round(33 + (temperature - 33)*(0.474 + 0.454*Math.Sqrt(wind) - 0.0454*wind), 2);
+        }
+
+        public static Dictionary<DateTime, double> GetWindChill()
+        {
+            Dictionary<DateTime, double> chills = new Dictionary<DateTime, double>();
+
+            using (var httpClient = new HttpClient())
+            {
+                var json =
+                    httpClient.GetStringAsync(
+                            "http://api.openweathermap.org/data/2.5/forecast?q=Leeuwarden,NL&units=metric&appid=c10147a59e2dbd59fcf9eea1557b2298").Result;
+                JObject jsonObject = JObject.Parse(json);
+
+                JArray forecasts = jsonObject["list"].ToObject<JArray>();
+                foreach (JObject forecast in forecasts)
+                {
+                    if (!forecast["wind"].HasValues)
+                    {
+                        break;
+                    }
+                    DateTime date = UnixTimeStampToDateTime(forecast["dt"].ToObject<int>());
+                    double temp = forecast["main"]["temp"].ToObject<double>();
+                    double wind = forecast["wind"]["speed"].ToObject<double>();
+                    chills.Add(date, WindChill(temp, wind));
+                }
+
+                return chills;
+            }
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
