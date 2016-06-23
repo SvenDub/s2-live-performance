7eb12e186de64f6b4c5e3bc16a2a7ca5c7fa1e24
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-06-23 15:17:01 +0200

    Export to JSON/XML

---
+++ Live Performance/Controllers/RentApiController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Inject;
+using Live_Performance.Entity;
+using Live_Performance.Persistence;
+
+namespace Live_Performance.Controllers
+{
+    [RoutePrefix("api/Rent")]
+    public class RentApiController : ApiController
+    {
+        private readonly IRepository<Rent> _repository = Injector.Resolve<IRepository<Rent>>();
+        private readonly IRepository<AreaRent> _areaRentRepository = Injector.Resolve<IRepository<AreaRent>>();
+        private readonly IRepository<ArticleRent> _articleRentRepository = Injector.Resolve<IRepository<ArticleRent>>();
+        private readonly IRepository<BoatRent> _boatRentRepository = Injector.Resolve<IRepository<BoatRent>>();
+
+        [Route("")]
+        [HttpGet]
+        public List<Rent> Index()
+        {
+            List<Rent> rents = _repository.FindAll();
+            rents.ForEach(rent =>
+            {
+                rent.Articles = _articleRentRepository.FindAllWhere(articleRent => articleRent.Rent == rent.Id);
+                rent.Boats = _boatRentRepository.FindAllWhere(boatRent => boatRent.Rent == rent.Id);
+                rent.Areas = _areaRentRepository.FindAllWhere(areaRent => areaRent.Rent == rent.Id);
+            });
+            return rents;
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public Rent Get(int id)
+        {
+            Rent rent = _repository.FindOne(id);
+            rent.Articles = _articleRentRepository.FindAllWhere(articleRent => articleRent.Rent == rent.Id);
+            rent.Boats = _boatRentRepository.FindAllWhere(boatRent => boatRent.Rent == rent.Id);
+            rent.Areas = _areaRentRepository.FindAllWhere(areaRent => areaRent.Rent == rent.Id);
+
+            return rent;
+        }
+    }
+}
--- Live Performance/Global.asax.cs
+++ Live Performance/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -27,6 +29,12 @@ namespace Live_Performance
                 .JsonFormatter
                 .SerializerSettings
                 .DefaultValueHandling = DefaultValueHandling.Include;
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
         }
     }
 }
